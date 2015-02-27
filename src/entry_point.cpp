34897388ed5a64b88ed32b6328aef6d51fd3cb29
diplay <diplaySuper@gmail.com>
2015-02-27 18:52:54 +0300

    more contrast

--- src/entry_point.cpp
+++ src/entry_point.cpp
@@ -24,7 +24,7 @@ int mainloop()
 	
 	if (core::beginRendering())
 	{		
-		Color clearColor(32, 32, 32, 255);
+		Color clearColor(16, 16, 16, 255);
 		Rect viewport(Point(0, 0), core::getDisplaySize());
 		//render all actors. Actor::render would be called also for all children
 		getStage()->render(clearColor, viewport);
